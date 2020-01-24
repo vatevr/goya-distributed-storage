db5e5b8137fa3a4bb684d9ba058580952a6b45f1
Hamlet Mkrtchyan <hamlet.mkrtchyan@bitpanda.com>
2020-01-24 17:42:31 +0100

    Intialized project skeleton

---
+++ manager/src/main/java/cc/univie/goya/manager/ManagerLauncher.java
@@ -0,0 +1,7 @@
+package cc.univie.goya.manager;
+
+public class ManagerLauncher {
+  public static void main(String[] args) {
+    System.out.print("Tititititriirir iri ");
+  }
+}
---
+++ node/src/main/java/cc/univie/goya/node/NodeLauncher.java
@@ -0,0 +1,7 @@
+package cc.univie.goya.node;
+
+public class NodeLauncher {
+  public static void main(String[] args) {
+    System.out.print("Buenos diags from node launchers");
+  }
+}
