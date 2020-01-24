4c42a7980500d2d1bb9a1935d106636206b85531
Hamlet Mkrtchyan <hamlet.mkrtchyan@bitpanda.com>
2020-01-25 00:05:42 +0100

    Put object endpoint for manager

--- manager/src/main/java/cc/univie/goya/manager/rest/VManagerHttp.java
+++ manager/src/main/java/cc/univie/goya/manager/rest/VManagerHttp.java
@@ -1,9 +1,13 @@
 package cc.univie.goya.manager.rest;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +19,15 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class VManagerHttp extends AbstractVerticle {
   private static final int DEFAULT_PORT = 8500;
+
+  private static int port(int hash) {
+    return DEFAULT_PORT + hash;
+  }
+
+  private static String hostname(int hash) {
+    return "node" + hash;
+  }
+
   private final int nodes;
   private WebClient webclient;
 
@@ -28,6 +41,7 @@ public class VManagerHttp extends AbstractVerticle {
 
     application.get("/hello").handler(this::handleHello);
     application.get("/ping-file").handler(this::handlePing);
+    application.post("/put/:objectKey").handler(this::handlePutObject);
 
     vertx.createHttpServer()
         .requestHandler(application)
@@ -40,12 +54,38 @@ public class VManagerHttp extends AbstractVerticle {
         });
   }
 
+  private void handlePutObject(RoutingContext context) {
+    String objectKey = context.pathParam("objectKey");
+    int hash = hashCode(objectKey);
+
+    this.sendToNode(hash).compose(response -> {
+      context.response().setStatusCode(200).end(response.encode());
+      return Future.succeededFuture();
+    })
+    .recover(err -> {
+      log.error("failed to put object to node {}", hash, err);
+      return Future.failedFuture(err);
+    });
+  }
+
+  private Future<JsonObject> sendToNode(int hash) {
+    Promise<HttpResponse<Buffer>> whenResponded = Promise.promise();
+
+    this.webclient
+        .post(port(hash), hostname(hash), "/ping")
+        .send(whenResponded);
+
+    return whenResponded.future().map(HttpResponse::bodyAsJsonObject);
+  }
+
+  private int hashCode(String objectKey) {
+    return objectKey.hashCode() % nodes;
+  }
+
   private void handlePing(RoutingContext context) {
     int hash = hash();
-    int port = DEFAULT_PORT + hash;
-    String hostname = "node" + hash;
 
-    this.webclient.get(port, hostname, "/ping").send(asyncResult -> {
+    this.webclient.get(port(hash), hostname(hash), "/ping").send(asyncResult -> {
       if (asyncResult.succeeded()) {
         context.response().setStatusCode(200).end(asyncResult.result().body());
       } else {
