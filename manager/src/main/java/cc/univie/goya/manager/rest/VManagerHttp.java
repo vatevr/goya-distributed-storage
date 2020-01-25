eebc73b9a4d20f7e853176cdc9d7e2633e71bbd3
Hamlet Mkrtchyan <hamlet.mkrtchyan@bitpanda.com>
2020-01-25 01:38:47 +0100

    API to search, upload and delete files from nodes

--- manager/src/main/java/cc/univie/goya/manager/rest/VManagerHttp.java
+++ manager/src/main/java/cc/univie/goya/manager/rest/VManagerHttp.java
@@ -1,13 +1,9 @@
 package cc.univie.goya.manager.rest;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +14,6 @@ import java.util.Random;
 @Slf4j
 @RequiredArgsConstructor
 public class VManagerHttp extends AbstractVerticle {
-  private static final int DEFAULT_PORT = 8500;
-
-  private static int port(int hash) {
-    return DEFAULT_PORT + hash;
-  }
-
-  private static String hostname(int hash) {
-    return "node" + hash;
-  }
-
   private final int nodes;
   private WebClient webclient;
 
@@ -39,9 +25,11 @@ public class VManagerHttp extends AbstractVerticle {
 
     Router application = Router.router(vertx);
 
-    application.get("/hello").handler(this::handleHello);
     application.get("/ping-file").handler(this::handlePing);
-    application.post("/put/:objectKey").handler(this::handlePutObject);
+
+    Gateway gateway = new Gateway(WebClient.create(vertx), nodes);
+    DictionaryApi api = new DictionaryApi(gateway);
+    api.attach(application);
 
     vertx.createHttpServer()
         .requestHandler(application)
@@ -54,38 +42,10 @@ public class VManagerHttp extends AbstractVerticle {
         });
   }
 
-  private void handlePutObject(RoutingContext context) {
-    String objectKey = context.pathParam("objectKey");
-    int hash = hashCode(objectKey);
-
-    this.sendToNode(hash).compose(response -> {
-      context.response().setStatusCode(200).end(response.encode());
-      return Future.succeededFuture();
-    })
-    .recover(err -> {
-      log.error("failed to put object to node {}", hash, err);
-      return Future.failedFuture(err);
-    });
-  }
-
-  private Future<JsonObject> sendToNode(int hash) {
-    Promise<HttpResponse<Buffer>> whenResponded = Promise.promise();
-
-    this.webclient
-        .post(port(hash), hostname(hash), "/ping")
-        .send(whenResponded);
-
-    return whenResponded.future().map(HttpResponse::bodyAsJsonObject);
-  }
-
-  private int hashCode(String objectKey) {
-    return objectKey.hashCode() % nodes;
-  }
-
   private void handlePing(RoutingContext context) {
     int hash = hash();
 
-    this.webclient.get(port(hash), hostname(hash), "/ping").send(asyncResult -> {
+    this.webclient.get(Gateway.port(hash), Gateway.host(hash), "/ping").send(asyncResult -> {
       if (asyncResult.succeeded()) {
         context.response().setStatusCode(200).end(asyncResult.result().body());
       } else {
@@ -100,11 +60,5 @@ public class VManagerHttp extends AbstractVerticle {
   private int hash() {
     Random random = new Random();
     return random.nextInt(nodes);
-  }
-
-  private void handleHello(RoutingContext context) {
-    log.info("hey there");
-
-    context.response().setStatusCode(200).end("holla");
   }
 }
